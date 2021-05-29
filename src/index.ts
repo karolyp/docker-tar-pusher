7f5942e72e84bfe901b77a8671f92af27d75857a
Károly Pákozdi <karoly.pakozdi.150@gmail.com>
2021-05-29 17:36:24 +0200

    Fixing index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
 import DockerTarPusher from './DockerTarPusher';
 import { DockerTarPusherOptions } from './types';
-
-const options: DockerTarPusherOptions = {
-  registryUrl: 'http://localhost:5000',
-  tarball: 'tars/hello.tar',
-  auth: {
-    username: 'testuser',
-    password: 'testpassword'
-  },
-  quiet: false
-};
-
-new DockerTarPusher(options).pushToRegistry();
-
-export { DockerTarPusher };
+export { DockerTarPusher, DockerTarPusherOptions };
