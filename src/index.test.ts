49c71c289ec46b408a887b819df33ca5dbb3b91f
Karoly Pakozdi <karoly@pkzd.hu>
2024-11-11 20:58:54 +0000

    ci: refactor test and add prettier and eslint

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,30 +2,28 @@ import { DockerTarPusher } from './index';
 import { execSync } from 'node:child_process';
 
 const image = 'busybox';
-const outputFile = '/tmp/image.tar.gz';
+const tarball = '/tmp/image.tar.gz';
+const registryUrl = process.env.REGISTRY_URL || 'http://localhost:5000';
 
 beforeAll(() => {
   const docker = process.env.GITHUB_ACTIONS ? 'docker' : 'podman';
   execSync(`${docker} pull ${image}:latest`);
-  execSync(`${docker} save ${image}:latest | gzip > ${outputFile}`);
+  execSync(`${docker} save ${image}:latest | gzip > ${tarball}`);
 });
 
 test('should upload image to registry', async () => {
   const dtp = new DockerTarPusher({
-    tarball: outputFile,
-    registryUrl: process.env.REGISTRY_URL!
+    tarball,
+    registryUrl
   });
 
-  // Assert that pushing the image to the registry does not throw
   await expect(dtp.pushToRegistry()).resolves.not.toThrow();
 
-  // Verify the image exists in the registry by querying the catalog
-  const result = await fetch(`${process.env.REGISTRY_URL}/v2/_catalog`, {
+  const result = await fetch(`${registryUrl}/v2/_catalog`, {
     method: 'GET'
   });
 
-  const json = await result.json();
+  const json = (await result.json()) as { repositories: string[] };
 
-  // Ensure the image is in the registry catalog
-  expect(json.repositories.some((repo: any) => repo.includes(image))).toBe(true);
+  expect(json.repositories).toEqual(expect.arrayContaining([expect.stringContaining(image)]));
 });
