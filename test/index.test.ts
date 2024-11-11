9b1da21494648699a1a03af84873ec857013c2dc
Karoly Pakozdi <karoly@pkzd.hu>
2024-11-11 17:19:21 +0000

    ci: add docker pull to test step

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,33 @@
 import { DockerTarPusher } from '../src';
+import { execSync } from 'node:child_process';
 
 describe('test main', () => {
+  const image = 'busybox';
+  const outputFile = '/tmp/image.tar.gz';
+
+  beforeAll(() => {
+    execSync(`podman pull ${image}:latest`);
+    execSync(`podman save ${image}:latest | gzip > ${outputFile}`);
+  });
+
   it('should upload image to registry', async () => {
     const dtp = new DockerTarPusher({
-      tarball: process.env.TARBALL_PATH!,
+      tarball: outputFile,
       registryUrl: process.env.REGISTRY_URL!
     });
 
     await expect(dtp.pushToRegistry()).resolves.not.toThrow();
+
+    const result = await fetch(`${process.env.REGISTRY_URL}/v2/_catalog`, {
+      method: 'GET'
+    });
+
+    const json = await result.json();
+
+    expect(json).toEqual(
+      expect.objectContaining({
+        repositories: expect.arrayContaining([expect.stringContaining(image)])
+      })
+    );
   });
 });
